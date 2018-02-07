49d4ee4b8aaf35860b6ee133d791238e456dffdd
iParaClient <iParaClient@iParaClient-PC>
2018-02-07 14:20:38 +0300

    [UPDATE]: Settings Update

--- Settings.php
+++ Settings.php
@@ -13,8 +13,8 @@ Bu sınıf örnek projemizde BaseController içerisinde kullanılmıştır. Ve t
      
         public function Settings()
         {
-            $this->PublicKey="3X2TECC0KO81PKG";//"Public Magaza Anahtarı - size mağaza başvurunuz sonucunda gönderilen publik key (açık anahtar) bilgisini kullanınız.",
-            $this->PrivateKey="3X2TECC0KO81PKGZTKCSO2ANJ";//"Private Magaza Anahtarı  - size mağaza başvurunuz sonucunda gönderilen privaye key (gizli anahtar) bilgisini kullanınız.",
+            $this->PublicKey="";//"Public Magaza Anahtarı - size mağaza başvurunuz sonucunda gönderilen publik key (açık anahtar) bilgisini kullanınız.",
+            $this->PrivateKey="";//"Private Magaza Anahtarı  - size mağaza başvurunuz sonucunda gönderilen privaye key (gizli anahtar) bilgisini kullanınız.",
             $this->BaseUrl="https://api.ipara.com/"; //iPara web servisleri API url'lerinin başlangıç bilgisidir. Restful web servis isteklerini takip eden kodlar halinde bulacaksınız.
                                                      // Örneğin "https://api.ipara.com/" + "/rest/payment/auth"  = "https://api.ipara.com/rest/payment/auth" 
             $this->Version="1.0";// Kullandığınız iPara API versiyonudur. 
